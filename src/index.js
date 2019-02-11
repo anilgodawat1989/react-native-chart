ae981a5f12c1d858bd31cd34c04d2987c2bfd77e
Anil <anil.godawat@appirio.com>
2019-02-11 17:46:16 +0530

    first commit

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+import { Text } from 'react-native';
+class Chart extends Component {
+  render() {
+    return <Text> Helllo Appirio </Text>;
+  }
+}
+
+export default ReactColorSquare;
